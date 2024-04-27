09508a0b0c30f1476320dd943e61d120edbcdfcc
vinicius-miguel <vinicius.de.o.mguel@gmail.com>
2024-04-26 21:19:35 -0300

    update js and footer

--- js/main.js
+++ js/main.js
@@ -5,12 +5,22 @@ navigator.geolocation.getCurrentPosition(position => {
 });
 
 function insertInHtml(json) {
-  document.getElementById("name-city").innerHTML = json.results.city;
-  document.getElementById("graus").innerHTML = json.results.temp + " °C";
-  document.getElementById("description").innerHTML = json.results.description;
-  document.getElementById("humidity").innerHTML = json.results.humidity + "%";
-  document.getElementById("wind").innerHTML = json.results.wind_speedy;
-  document.getElementById("day-0").innerHTML = json.results.forecast[0].weekday;
+  const city = "Cidade: " + json.results.city;
+  const temp = "Temperatura: " + json.results.temp + " °C";
+  const description = "Descrição do Tempo: " + json.results.description;
+  const humidity = "Humidade: " + json.results.humidity + "%";
+  const wind = "Vento: " + json.results.wind_speedy + " km/h";
+  const todayTemp = "Hoje: " + json.results.forecast[0].min + "° - " + json.results.forecast[0].max + "°";
+
+  // Atualize os campos fixos com os valores correspondentes
+  document.getElementById("name-city").innerHTML = city;
+
+  // Adicione as informações pesquisadas na frente dos campos fixos
+  document.getElementById("graus").innerHTML = temp;
+  document.getElementById("description").innerHTML = description;
+  document.getElementById("humidity").innerHTML = humidity;
+  document.getElementById("wind").innerHTML = wind;
+  document.getElementById("day-0").innerHTML = todayTemp;
 }
 
 function searchClimate(latitude, longitude) {
@@ -24,12 +34,32 @@ function searchClimate(latitude, longitude) {
     .catch(err => console.log("Erro ao buscar dados: ", err));
 }
 
-function buscarPrevisão() {
+function buscarPrevisão(type = "agora") {
   const city = document.getElementById("city-input").value;
-  const state = ""; // O estado não é fornecido pelo usuário no seu HTML, então está vazio aqui
-  const apiKey = "9a8f50a0"; // Substitua "SUA-CHAVE" pela sua chave da API
+  const state = "";
+  const apiKey = "9a8f50a0"; 
+  let apiUrl;
 
-  const apiUrl = `https://api.hgbrasil.com/weather?key=${apiKey}&city_name=${city},${state}`;
+  switch (type) {
+    case "agora":
+      apiUrl =  `https://api.hgbrasil.com/weather?key=${apiKey}&city_name=${city},${state}`;
+      break;
+    case "hoje":
+      // Lógica para buscar previsão do dia atual
+      apiUrl =  `https://api.hgbrasil.com/weather?key=${apiKey}&city_name=${city},${state}&forecast_only=true`;
+      break;
+    case "fim-de-semana":
+      // Lógica para buscar previsão do fim de semana
+      apiUrl =  `https://api.hgbrasil.com/weather?key=${apiKey}&city_name=${city},${state}&filter=weekend`;
+      break;
+    case "15-dias":
+      // Lógica para buscar previsão dos próximos 15 dias
+      apiUrl =  `https://api.hgbrasil.com/weather?key=${apiKey}&city_name=${city},${state}&forecast_days=15`;
+      break;
+    default:
+      console.log("Tipo de previsão não reconhecido");
+      return;
+  }
 
   fetch(proxyUrl + apiUrl)
     .then(res => res.json())
@@ -42,18 +72,33 @@ function buscarPrevisão() {
 
 function handleSearch(event) {
   if (event.key === "Enter") {
-    buscarPrevisão();
+    buscarPrevisão("agora");
   }
 }
 
-// Adiciona o evento de pesquisa ao pressionar "Enter"
 document.getElementById("city-input").addEventListener("keypress", handleSearch);
 
-// Remova o atributo wm-submit do seu botão de busca
+document.getElementById("agora-btn").addEventListener("click", function() {
+  buscarPrevisão("agora");
+});
+
+document.getElementById("hoje-btn").addEventListener("click", function() {
+  buscarPrevisão("hoje");
+});
+
+document.getElementById("fim-de-semana-btn").addEventListener("click", function() {
+  buscarPrevisão("fim-de-semana");
+});
+
+document.getElementById("15-dias-btn").addEventListener("click", function() {
+  buscarPrevisão("15-dias");
+});
+
 const submit = document.querySelector(".search-btn");
 submit.onclick = function(e) {
   e.preventDefault();
   buscarPrevisão();
 }
+
 
 
